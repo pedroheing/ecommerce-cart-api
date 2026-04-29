f48a6ee26077167b3bc902adb5e054cb6eb474f9
Pedro Henrique Heing Geronimo <pedroheing@gmail.com>
2026-04-29 15:27:27 -0300

    feat: adds ttl to dynamo cart

--- src/main/java/com/pedroheing/shoppingcart/cart/CartDynamoProperties.java
+++ src/main/java/com/pedroheing/shoppingcart/cart/CartDynamoProperties.java
@@ -2,12 +2,16 @@ package com.pedroheing.shoppingcart.cart;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
+
+import java.time.Duration;
 
 @Validated
 @ConfigurationProperties(prefix = "shoppingcart.cart.dynamo")
 public record CartDynamoProperties(
         @NotBlank String tableName,
-        @Min(1) int batchSize
+        @Min(1) int batchSize,
+        @NotNull Duration ttl
 ) {}
